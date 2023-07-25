fb823490d332e8eea8e0df2809132d6d98aa70c8
naseeb0 <nasheeb.dangi@gmail.com>
2023-07-25 14:11:32 +0545

    AREA AND PERMETER

--- app/src/main/java/com/example/simpleinterst/MainActivity.java
+++ app/src/main/java/com/example/simpleinterst/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.simpleinterst;
 
-import android.os.Bundle;
-
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.FragmentTransaction;
+import android.app.Dialog;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,12 +15,52 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        if (savedInstanceState == null) {
-            
-            InterestCalculatorFragment fragment = new InterestCalculatorFragment();
-            FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-            transaction.add(R.id.fragmentContainer, fragment);
-            transaction.commit();
-        }
+        Button calculateButton = findViewById(R.id.buttonCalculate);
+        calculateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showInputDialog();
+            }
+        });
+    }
+
+    private void showInputDialog() {
+        final Dialog dialog = new Dialog(this);
+        dialog.setContentView(R.layout.dialog_input);
+        dialog.setTitle("Rectangle Calculator");
+
+        final EditText editTextLength = dialog.findViewById(R.id.editTextLength);
+        final EditText editTextWidth = dialog.findViewById(R.id.editTextWidth);
+        Button calculateButton = dialog.findViewById(R.id.buttonCalculate);
+
+        calculateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                double length = Double.parseDouble(editTextLength.getText().toString());
+                double width = Double.parseDouble(editTextWidth.getText().toString());
+
+                double area = length * width;
+                double perimeter = 2 * (length + width);
+
+                showResultDialog(area, perimeter);
+                dialog.dismiss();
+            }
+        });
+
+        dialog.show();
+    }
+
+    private void showResultDialog(double area, double perimeter) {
+        Dialog resultDialog = new Dialog(this);
+        resultDialog.setContentView(R.layout.dialog_result);
+        resultDialog.setTitle("Result");
+
+        TextView textViewArea = resultDialog.findViewById(R.id.textViewArea);
+        TextView textViewPerimeter = resultDialog.findViewById(R.id.textViewPerimeter);
+
+        textViewArea.setText("Area: " + area);
+        textViewPerimeter.setText("Perimeter: " + perimeter);
+
+        resultDialog.show();
     }
 }
