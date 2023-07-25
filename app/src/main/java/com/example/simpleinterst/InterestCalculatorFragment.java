19e47c1bd55d9aec559aeed025d5ac553848e0bd
naseeb0 <nasheeb.dangi@gmail.com>
2023-07-25 14:03:00 +0545

    SI

---
+++ app/src/main/java/com/example/simpleinterst/InterestCalculatorFragment.java
@@ -0,0 +1,56 @@
+package com.example.simpleinterst;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class InterestCalculatorFragment extends Fragment {
+
+    private EditText editTextPrincipal;
+    private EditText editTextInterestRate;
+    private EditText editTextTimePeriod;
+    private TextView textViewResult;
+
+    public InterestCalculatorFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View rootView = inflater.inflate(R.layout.fragment_interest_calculator, container, false);
+
+        editTextPrincipal = rootView.findViewById(R.id.editTextPrincipal);
+        editTextInterestRate = rootView.findViewById(R.id.editTextInterestRate);
+        editTextTimePeriod = rootView.findViewById(R.id.editTextTimePeriod);
+        textViewResult = rootView.findViewById(R.id.textViewResult);
+
+        Button buttonCalculate = rootView.findViewById(R.id.buttonCalculate);
+        buttonCalculate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                calculateInterest();
+            }
+        });
+
+        return rootView;
+    }
+
+    private void calculateInterest() {
+        double principal = Double.parseDouble(editTextPrincipal.getText().toString());
+        double interestRate = Double.parseDouble(editTextInterestRate.getText().toString());
+        double timePeriod = Double.parseDouble(editTextTimePeriod.getText().toString());
+
+        double simpleInterest = (principal * interestRate * timePeriod) / 100.0;
+        textViewResult.setText(String.format("Simple Interest: %.2f", simpleInterest));
+    }
+}
--- app/src/main/java/com/example/simpleinterst/MainActivity.java
+++ app/src/main/java/com/example/simpleinterst/MainActivity.java
@@ -1,92 +1,23 @@
 package com.example.simpleinterst;
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
 
 public class MainActivity extends AppCompatActivity {
-
-    private EditText editTextResult;
-    private double operand1 = Double.NaN;
-    private String operator = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        editTextResult = findViewById(R.id.editTextResult);
-    }
-
-    public void onDigitClick(View view) {
-        Button button = (Button) view;
-        editTextResult.append(button.getText().toString());
-    }
-
-    public void onDecimalClick(View view) {
-        if (!editTextResult.getText().toString().contains(".")) {
-            editTextResult.append(".");
-        }
-    }
-
-    public void onOperatorClick(View view) {
-        Button button = (Button) view;
-        operator = button.getText().toString();
-        operand1 = Double.parseDouble(editTextResult.getText().toString());
-        editTextResult.setText("");
-    }
-
-    public void onClearClick(View view) {
-        editTextResult.setText("");
-        operand1 = Double.NaN;
-        operator = "";
-    }
-
-    public void onPlusMinusClick(View view) {
-        if (!editTextResult.getText().toString().isEmpty()) {
-            double value = Double.parseDouble(editTextResult.getText().toString());
-            editTextResult.setText(String.valueOf(-value));
-        }
-    }
-
-    public void onPercentClick(View view) {
-        if (!editTextResult.getText().toString().isEmpty()) {
-            double value = Double.parseDouble(editTextResult.getText().toString()) / 100.0;
-            editTextResult.setText(String.valueOf(value));
-        }
-    }
-
-    public void onEqualsClick(View view) {
-        if (!operator.isEmpty()) {
-            String secondOperandStr = editTextResult.getText().toString();
-            if (!secondOperandStr.isEmpty()) {
-                double secondOperand = Double.parseDouble(secondOperandStr);
-                double result = calculateResult(operand1, secondOperand, operator);
-                editTextResult.setText(String.valueOf(result));
-                operator = "";
-                operand1 = Double.NaN;
-            }
-        }
-    }
-
-    private double calculateResult(double operand1, double operand2, String operator) {
-        switch (operator) {
-            case "+":
-                return operand1 + operand2;
-            case "-":
-                return operand1 - operand2;
-            case "*":
-                return operand1 * operand2;
-            case "/":
-                if (operand2 == 0) {
-                    return Double.NaN; // Division by zero
-                }
-                return operand1 / operand2;
-            default:
-                return Double.NaN; // Invalid operator
+        if (savedInstanceState == null) {
+            
+            InterestCalculatorFragment fragment = new InterestCalculatorFragment();
+            FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+            transaction.add(R.id.fragmentContainer, fragment);
+            transaction.commit();
         }
     }
 }
